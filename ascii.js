94ba4401981c9d2345f2b274e4b0197ce397c9c1
gfalbarracinr <gfalbarracinr@unal.edu.co>
2020-04-27 11:32:24 -0500

    adding ascci js file, with all changes

--- ascii.js
+++ ascii.js
@@ -5,74 +5,57 @@ var options = [' ','`','.',',-',"':",';_~','"','*|','!l',
 '3u','2Ckty{','jn','4FVY','5P[]af','qw','Sde','Eo',
 'NOZ','9HXgh','GTU','$AIm','QW','KM','%8','#06@','bp',
 'D','&','R','B'];
+var len = options.length - 1;
 var gui,btn,livebtn;
 var live = false;
+var filter = false;
 var capture;
 var pg;
 
 function setup() {
   //paragraph for display of ascii result
   resdiv = createP('');
-  
+  resdiv.style('font-family', '"Courier new", Courier, monospace');
+  resdiv.style('font-size', '10');
   //video capture elements
-  pg = createGraphics(160,120);
+  pg = createGraphics(240,120);
   capture = createCapture(VIDEO);
-  capture.size(80, 60);
+  capture.size(240, 120);
   capture.hide();
-  
+ 
   //gui elements
-  btn = createButton('UPDATE');
-  btn.mousePressed(calcCapture);
   gui = createDiv('');
-  btn.parent(gui);
   livebtn = createButton('LIVE');
   livebtn.mousePressed(function(){live=!live;});
   livebtn.parent(gui);
-  
-  cnv = createCanvas(400,300);
+  filterbtn = createButton('CHANGE FILTER');
+  filterbtn.parent(gui);
+  filterbtn.style('display', 'none');
+  filterbtn.mousePressed(function(){options=options.reverse();});
+  cnv = createCanvas(240,120);
   background(255);
-
-  img = loadImage('data/charlize_level.jpg', function(pic){
-    calcImg(pic);
-  });
 }
 
 function draw() {
   image(capture, 0, 0, width, height);
-  if (live) calcCapture();
-}
-
-function calcImg(pic) {
-  var res = '<pre>';
-  for (var i=0; i<60; i++) {
-    var line = '';
-    for (var j=0; j<140; j++) {
-      var x = pic.get(2+round(j*5.714),5+i*10);
-      var v = round((1-x[0]/255.0)*40);
-      var index = floor(random(options[v].length));
-      var chr = options[v][index];
-      if (chr==' ') chr='&nbsp;';
-      if (chr=='<') chr='&lt;';
-      if (chr=='>') chr='&gt;';
-      if (chr=='"') chr='&quot;';
-      line += chr;
-    }
-    res += line+'<br>';
+  if (live) {
+    cnv.style('position', 'fixed');
+    cnv.style('top', '0');
+    filterbtn.style('display', 'block');
+    calcCapture();
   }
-  res += '</pre>'
-  resdiv.html(res);
 }
 
 function calcCapture() {
-  pg.image(capture,0,0,80,60);
+  pg.image(capture,0,0,240,120);
   var res = '<pre>';
   for (var i=0; i<60; i++) {
     var line = '';
-    for (var j=0; j<140; j++) {
+    for (var j=0; j<200; j++) {
       var x = pg.get(round(j*1.143),i*2);
       var f = (1-x[0]/255.0);
       f = f*f; //square factor to lighten up, because less bright characters
-      var v = round(f*40);
+      var v = round(f*len);
       var index = floor(random(options[v].length));
       var chr = options[v][index];
       if (chr==' ') chr='&nbsp;';
@@ -84,6 +67,6 @@ function calcCapture() {
 
     res += line+'<br>';
   }
-  res += '</pre>'
+  res += '</pre>';
   resdiv.html(res);
 }
